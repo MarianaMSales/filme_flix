253907d58bc3bdda5eca0c483b933647f4878bbb
Mariana Sales <mariana.sales@cwi.com.br>
2025-05-05 15:20:48 -0300

    commit inicial

---
+++ android/app/src/main/kotlin/com/example/filme_flix_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.filme_flix_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/landing_page.dart
@@ -0,0 +1,70 @@
+import 'package:filme_flix_app/components/primary_button.dart';
+import 'package:filme_flix_app/components/secondary_button.dart';
+import 'package:flutter/material.dart';
+
+class LandingPage extends StatelessWidget {
+  const LandingPage({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Material(
+      child: Stack(
+        alignment: Alignment.bottomCenter,
+        children: [
+          Positioned.fill(
+            child: Image.asset(
+              'assets/images/the_last_jedi.jpg',
+              fit: BoxFit.cover,
+            ),
+          ),
+          Container(
+            height: size.height * .33,
+            width: size.width,
+            padding: const EdgeInsets.symmetric(
+              horizontal: 19,
+              vertical: 29,
+            ),
+            decoration: BoxDecoration(
+              color: Colors.black.withOpacity(.8),
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(50),
+                topRight: Radius.circular(50),
+              ),
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Text(
+                  "Watch movies anytime anywhere",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                ),
+                const Text(
+                  "Explore a vast collection of blockbuster movies, timeless classics, and the latest releases.",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 13,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                PrimaryButton(
+                  onPressed: () {},
+                  text: "Login",
+                ),
+                SecondaryButton(
+                  onPressed: () {},
+                  text: "Sign Up",
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
